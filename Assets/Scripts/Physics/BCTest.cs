3a84641c72ea6f0aca8535a5349826e97a546840
caleb <calebkhiebert@gmail.com>
2016-03-29 09:48:07 -0500

    box collider

---
+++ Assets/Scripts/Physics/BCTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using BoxCollider = Assets.Scripts.Physics.BoxCollider;
+
+public class BCTest : MonoBehaviour
+{
+
+    private BoxCollider _collider;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _collider = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var point = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+	    if (_collider.ContainsPoint(point))
+	    {
+	        Debug.Log("WOLOLOLOL");
+	    }
+	}
+}
---
+++ Assets/Scripts/Physics/BoxCollider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Physics
+{
+    /// <summary>
+    /// This is essentially just an info container, this will contain some basic info
+    /// This class will also contian some methods for collision detection
+    /// </summary>
+    [ExecuteInEditMode]
+    public class BoxCollider : MonoBehaviour
+    {
+
+        // the size of the box in world units
+        public Vector2 Size;
+
+        // the local positional offset
+        public Vector2 Position;
+
+        /// <summary>
+        /// This is called in the unity editor
+        /// It allows you to draw lines and other such things in the editor window
+        /// </summary>
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube((Vector2)transform.position + Position, Size);
+        }
+
+        /// <summary>
+        /// Checks whether or not a given point is inside this box
+        /// </summary>
+        /// <param name="point">The point to check</param>
+        /// <returns>Whether or not the input point is inside the box</returns>
+        public bool ContainsPoint(Vector2 point)
+        {
+            return point.x > transform.position.x + Position.x - Size.x/2 &&
+                   point.x < transform.position.x + Position.x + Size.x/2 &&
+                   point.y > transform.position.x + Position.y - Size.y/2 &&
+                   point.y < transform.position.x + Position.y + Size.y/2;
+        }
+    }
+}
